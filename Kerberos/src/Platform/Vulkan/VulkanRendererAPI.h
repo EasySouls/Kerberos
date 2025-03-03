1973451ea43026ea8f981d80bf76c07f5e1da49b
Tarjányi Bence Csanád <tarjanyicsanad@gmail.com>
2025-03-03 19:56:18 +0100

    Create pipeline with the Vulkan rendering API

--- Kerberos/src/Platform/Vulkan/VulkanRendererAPI.h
+++ Kerberos/src/Platform/Vulkan/VulkanRendererAPI.h
@@ -10,7 +10,7 @@ namespace Kerberos
 	{
 	public:
 		void Init() override;
-		void SetupPipeline();
+		void Cleanup() const;
 
 		void SetViewport(const uint32_t x, const uint32_t y, const uint32_t width, const uint32_t height) override;
 
@@ -20,7 +20,17 @@ namespace Kerberos
 		void DrawIndexed(const Ref<VertexArray>& vertexArray) override;
 
 	private:
+		void CreateRenderPass();
+		void CreateGraphicsPipeline();
+
+	private:
+		VkDevice m_Device = VK_NULL_HANDLE;
 		VkCommandBuffer m_CommandBuffer = VK_NULL_HANDLE;
 		VkImage m_Image = VK_NULL_HANDLE;
+		VkExtent2D m_SwapChainExtent = {};
+		VkFormat m_SwapChainImageFormat = {};
+		VkRenderPass m_RenderPass = VK_NULL_HANDLE;
+		VkPipelineLayout m_PipelineLayout = VK_NULL_HANDLE;
+		VkPipeline m_GraphicsPipeline = VK_NULL_HANDLE;
 	};
 }
