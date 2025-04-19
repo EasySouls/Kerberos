4e356d4ed51dd27da56022236eb38880f8e7b714
Tarjányi Bence Csanád <tarjanyicsanad@gmail.com>
2025-04-19 23:11:49 +0200

    Implementin Camera Component UI and add ability to change between orthographic and perspective

--- Kerberos/src/Kerberos/Scene/SceneCamera.cpp
+++ Kerberos/src/Kerberos/Scene/SceneCamera.cpp
@@ -5,13 +5,15 @@
 
 namespace Kerberos
 {
-	SceneCamera::SceneCamera() 
+	SceneCamera::SceneCamera()
 	{
 		RecalculateProjection();
 	}
 
-	void SceneCamera::SetOrthographic(const float size, const float nearClip, const float farClip) 
+	void SceneCamera::SetOrthographic(const float size, const float nearClip, const float farClip)
 	{
+		m_ProjectionType = ProjectionType::Orthographic;
+
 		m_OrthoSize = size;
 		m_OrthoNear = nearClip;
 		m_OrthoFar = farClip;
@@ -19,20 +21,39 @@ namespace Kerberos
 		RecalculateProjection();
 	}
 
-	void SceneCamera::SetViewportSize(const uint32_t width, const uint32_t height) 
+	void SceneCamera::SetPerspective(const float fov, const float nearClip, const float farClip)
+	{
+		m_ProjectionType = ProjectionType::Perspective;
+
+		m_PerspectiveFov = fov;
+		m_PerspectiveNear = nearClip;
+		m_PerspectiveFar = farClip;
+
+		RecalculateProjection();
+	}
+
+	void SceneCamera::SetViewportSize(const uint32_t width, const uint32_t height)
 	{
 		m_AspectRatio = static_cast<float>(width) / static_cast<float>(height);
 
 		RecalculateProjection();
 	}
 
-	void SceneCamera::RecalculateProjection() 
+	void SceneCamera::RecalculateProjection()
 	{
-		const float orthoLeft = -m_OrthoSize * m_AspectRatio * 0.5f;
-		const float orthoRight = m_OrthoSize * m_AspectRatio * 0.5f;
-		const float orthoBottom = -m_OrthoSize * 0.5f;
-		const float orthoTop = m_OrthoSize * 0.5f;
+		if (m_ProjectionType == ProjectionType::Orthographic)
+		{
+			const float orthoLeft = -m_OrthoSize * m_AspectRatio * 0.5f;
+			const float orthoRight = m_OrthoSize * m_AspectRatio * 0.5f;
+			const float orthoBottom = -m_OrthoSize * 0.5f;
+			const float orthoTop = m_OrthoSize * 0.5f;
 
-		m_Projection = glm::ortho(orthoLeft, orthoRight, orthoBottom, orthoTop, m_OrthoNear, m_OrthoFar);
+			m_Projection = glm::ortho(orthoLeft, orthoRight, orthoBottom, orthoTop, m_OrthoNear, m_OrthoFar);
+		}
+		else
+		{
+			m_Projection = glm::perspective(m_PerspectiveFov, m_AspectRatio, m_PerspectiveNear, m_PerspectiveFar);
+			m_Projection[1][1] *= -1; // Invert Y axis
+		}
 	}
 }
--- KerberosEditor/src/Editor/HierarchyPanel.cpp
+++ KerberosEditor/src/Editor/HierarchyPanel.cpp
@@ -25,6 +25,7 @@ namespace Kerberos
 		}
 		ImGui::End();
 
+		/// If an empty space is clicked, deselect the entity
 		if (ImGui::IsMouseDown(0) && ImGui::IsWindowHovered())
 		{
 			m_SelectedEntity = {};
@@ -63,31 +64,113 @@ namespace Kerberos
 	{
 		if (entity.HasComponent<TagComponent>())
 		{
-			auto& tag = entity.GetComponent<TagComponent>().Tag;
-
-			char buffer[256];
-			strcpy_s(buffer, sizeof(buffer), tag.c_str());
-			if (ImGui::InputText("Tag", buffer, sizeof(buffer)))
+			if (ImGui::TreeNodeEx(reinterpret_cast<void*>(typeid(TagComponent).hash_code()), ImGuiTreeNodeFlags_DefaultOpen, "Tag"))
 			{
-				tag = buffer;
+				auto& tag = entity.GetComponent<TagComponent>().Tag;
+
+				char buffer[256];
+				strcpy_s(buffer, sizeof(buffer), tag.c_str());
+				if (ImGui::InputText("Tag", buffer, sizeof(buffer)))
+				{
+					tag = buffer;
+				}
+
+				ImGui::TreePop();
 			}
+			
 		}
 		if (entity.HasComponent<TransformComponent>())
 		{
-			auto& transform = entity.GetComponent<TransformComponent>();
-			ImGui::Text("Transform");
-			ImGui::DragFloat3("Position", &transform.Transform[3][0], 0.1f);
-			ImGui::DragFloat3("Rotation", &transform.Transform[2][0], 0.1f);
-			ImGui::DragFloat3("Scale", &transform.Transform[1][0], 0.1f);
+			if (ImGui::TreeNodeEx(reinterpret_cast<void*>(typeid(TransformComponent).hash_code()), ImGuiTreeNodeFlags_DefaultOpen, "Transform"))
+			{
+				auto& transform = entity.GetComponent<TransformComponent>();
+				ImGui::DragFloat3("Position", &transform.Transform[3][0], 0.1f);
+				ImGui::DragFloat3("Rotation", &transform.Transform[2][0], 0.1f);
+				ImGui::DragFloat3("Scale", &transform.Transform[1][0], 0.1f);
+
+				ImGui::TreePop();
+			}
 		}
+		if (entity.HasComponent<CameraComponent>())
+		{
+			if (ImGui::TreeNodeEx(reinterpret_cast<void*>(typeid(CameraComponent).hash_code()), ImGuiTreeNodeFlags_DefaultOpen, "Camera"))
+			{
+				auto& cameraComponent = entity.GetComponent<CameraComponent>();
+				auto& camera = cameraComponent.Camera;
+
+				const char* projectionTypeStrings[] = { "Perspective", "Orthographic" };
+				const char* currentProjectionTypeString = projectionTypeStrings[static_cast<int>(camera.GetProjectionType())];
+
+				if (ImGui::BeginCombo("Projection Type", currentProjectionTypeString))
+				{
+					for (int i = 0; i < 2; i++)
+					{
+						const bool isSelected = (currentProjectionTypeString == projectionTypeStrings[i]);
+						if (ImGui::Selectable(projectionTypeStrings[i], isSelected))
+						{
+							camera.SetProjectionType(static_cast<SceneCamera::ProjectionType>(i));
+						}
+						if (isSelected)
+						{
+							ImGui::SetItemDefaultFocus();
+						}
+					}
+
+					ImGui::EndCombo();
+				}
+
+				if (camera.GetProjectionType() == SceneCamera::ProjectionType::Orthographic)
+				{
+					float size = camera.GetOrthographicSize();
+					if (ImGui::DragFloat("Size", &size))
+					{
+						camera.SetOrthographicSize(size);
+					}
+					float nearClip = camera.GetOrthographicNearClip();
+					if (ImGui::DragFloat("Near Clip", &nearClip))
+					{
+						camera.SetOrthographicNearClip(nearClip);
+					}
+					float farClip = camera.GetOrthographicFarClip();
+					if (ImGui::DragFloat("Far Clip", &farClip))
+					{
+						camera.SetOrthographicFarClip(farClip);
+					}
+				}
+				else
+				{
+					float fov = glm::degrees(camera.GetPerspectiveFov());
+					if (ImGui::DragFloat("FOV", &fov))
+					{
+						camera.SetPerspectiveFov(glm::radians(fov));
+					}
+					float nearClip = camera.GetPerspectiveNearClip();
+					if (ImGui::DragFloat("Near Clip", &nearClip))
+					{
+						camera.SetPerspectiveNearClip(nearClip);
+					}
+					float farClip = camera.GetPerspectiveFarClip();
+					if (ImGui::DragFloat("Far Clip", &farClip))
+					{
+						camera.SetPerspectiveFarClip(farClip);
+					}
+				}
+
+				ImGui::Checkbox("Primary", &cameraComponent.IsPrimary);
+				ImGui::Checkbox("Fixed Aspect Ratio", &cameraComponent.FixedAspectRatio);
+
+				ImGui::TreePop();
+			}
+		}	
 		if (entity.HasComponent<SpriteRendererComponent>())
 		{
-			auto& spriteRenderer = entity.GetComponent<SpriteRendererComponent>();
-			ImGui::Text("Sprite");
-			ImGui::ColorEdit4("Red", &spriteRenderer.Color.r);
-			ImGui::ColorEdit4("Green", &spriteRenderer.Color.g);
-			ImGui::ColorEdit4("Blue", &spriteRenderer.Color.b);
-			ImGui::ColorEdit4("Alpha", &spriteRenderer.Color.a);
+			if (ImGui::TreeNodeEx(reinterpret_cast<void*>(typeid(SpriteRendererComponent).hash_code()), ImGuiTreeNodeFlags_DefaultOpen, "Sprite"))
+			{
+				auto& spriteRenderer = entity.GetComponent<SpriteRendererComponent>();
+				ImGui::ColorEdit4("Color", &spriteRenderer.Color[0]);
+
+				ImGui::TreePop();
+			}
 		}
 	}
 }
