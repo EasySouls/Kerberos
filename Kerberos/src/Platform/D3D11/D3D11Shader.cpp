f6964fd7f66c0f5e7ac9a20b58048f3b4e1eff3d
Tarjányi Bence Csanád <tarjanyicsanad@gmail.com>
2025-05-27 17:13:08 +0200

    Start working on D3D11 shaders

--- Kerberos/src/Platform/D3D11/D3D11Shader.cpp
+++ Kerberos/src/Platform/D3D11/D3D11Shader.cpp
@@ -1,7 +1,34 @@
 #include "kbrpch.h"
 #include "D3D11Shader.h"
 
+#include <d3dcompiler.h>
+
 namespace Kerberos
 {
+	D3D11Shader::D3D11Shader(const std::string& filepath) {}
 
+	D3D11Shader::D3D11Shader(std::string name, const std::string& vertexSrc, const std::string& fragmentSrc,
+		const std::string& geometrySrc) {}
+
+	D3D11Shader::~D3D11Shader() {}
+	void D3D11Shader::Bind() const {}
+	void D3D11Shader::Unbind() const {}
+
+	bool D3D11Shader::CompileShader(const std::wstring& fileName, const std::string& entryPoint,
+		const std::string& profile, ComPtr<ID3DBlob>& shaderBlob) const 
+	{
+		constexpr UINT compileFlags = D3DCOMPILE_ENABLE_STRICTNESS;
+
+		ComPtr<ID3DBlob> tempShaderBlob;
+		ComPtr<ID3DBlob> errorBlob;
+
+		HRESULT hr = D3DCompileFromFile(fileName.data(),
+			nullptr, D3D_COMPILE_STANDARD_FILE_INCLUDE, entryPoint.c_str(), profile.c_str(),
+			compileFlags, 0, &tempShaderBlob, &errorBlob);
+	}
+
+	ComPtr<ID3D11VertexShader> D3D11Shader::CreateVertexShader(const std::wstring& fileName,
+	                                                           ComPtr<ID3DBlob>& vertexShaderBlob) const {}
+
+	ComPtr<ID3D11PixelShader> D3D11Shader::CreatePixelShader(std::wstring& fileName) const {}
 }
--- Kerberos/src/Platform/D3D11/D3D11Shader.h
+++ Kerberos/src/Platform/D3D11/D3D11Shader.h
@@ -1,10 +1,33 @@
 #pragma once
+
 #include "Kerberos/Renderer/Shader.h"
+
+#include <d3d11.h>
+#include <wrl.h>
 
 namespace Kerberos
 {
+	using Microsoft::WRL::ComPtr;
+
 	class D3D11Shader : public Shader
 	{
+	public:
+		explicit D3D11Shader(const std::string& filepath);
+		D3D11Shader(std::string name, const std::string& vertexSrc, const std::string& fragmentSrc, const std::string& geometrySrc = "");
+		~D3D11Shader() override;
+
+		void Bind() const override;
+		void Unbind() const override;
+
+	private:
+		bool CompileShader(const std::wstring& fileName, const std::string& entryPoint, const std::string& profile, ComPtr<ID3DBlob>& shaderBlob) const;
+
+		[[nodiscard]] 
+		ComPtr<ID3D11VertexShader> CreateVertexShader(const std::wstring& fileName, ComPtr<ID3DBlob>& vertexShaderBlob) const;
+
+		[[nodiscard]] 
+		ComPtr<ID3D11PixelShader> CreatePixelShader(std::wstring& fileName) const;
+
 	};
 }
 
