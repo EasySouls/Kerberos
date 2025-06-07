821bb2d47f997484dbeaa36dc4c721e2dbac985c
Tarjányi Bence Csanád <tarjanyicsanad@gmail.com>
2025-06-07 16:07:44 +0200

    Remove IMGUI_IMPL_API define from ImGuiLayer.cpp

--- Kerberos/src/Kerberos/ImGui/ImGuiLayer.cpp
+++ Kerberos/src/Kerberos/ImGui/ImGuiLayer.cpp
@@ -3,7 +3,6 @@
 #include "Kerberos/Application.h"
 
 #include <imgui/imgui.h>
-//#define IMGUI_IMPL_API
 #include <imgui/backends/imgui_impl_glfw.h>
 #include <imgui/backends/imgui_impl_opengl3.h>
 #include <imgui/backends/imgui_impl_vulkan.h>
